a665b6058189708f35308de923a03c4d34d8125f
Joseph Lamb <jlambpco@gmail.com>
2022-10-15 16:31:31 -0400

    background imaged added with image blend and wireframe created in bootstrap. Working on content and formatting

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,19 +1,32 @@
 
-
+// searchLoc = $("#search").val()
+// apiKey = '20f82cbfb698e805cd598e366c3108b2'
+// fetchAPI = '"http://api.openweathermap.org/data/2.5/forecast?q='+searchLoc+'&units=imperial&appid='+apiKey+'"'
 //LIVE API
-// fetch("http://api.openweathermap.org/data/2.5/forecast?q="+searchLoc+"&units=imperial&appid=20f82cbfb698e805cd598e366c3108b2")
-
+// fetch(fetchAPI)
+dailyForecast = [];
 //TEST API
 fetch("./assets/test.JSON")
 
 .then ((response) => response.json())
-.then (data => {
+.then (function (data) {
     console.log(data);
-    var nowTemp
-    var nowWeather
-    var nowWeatherIcon
-    var nowHumidity
-    var nowClouds
-    var nowDateTm
-    
-})
+    for (t=0; t<=40; t+=8) 
+    {
+        weather = {
+            day: t,
+            temp: data.list[t].main.temp,
+            weather: data.list[t].weather[0].main,
+            weatherIcon: data.list[t].weather[0].icon,
+            weatherDtl: data.list[t].weather[0].description,
+            humidity: data.list[t].main.humidity,
+            heatIndex: data.list[t].main.feels_like,
+            low: data.list[t].main.temp_min,
+            high: data.list[t].main.temp_max,
+            wind: data.list[t].wind.speed
+            }
+    dailyForecast.push(weather);
+    }
+    return dailyForecast;}
+)
+console.log(dailyForecast)
