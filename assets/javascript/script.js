01baec766309dec4bddd7d2e999a7184bd4a5e1b
Joseph Lamb <jlambpco@gmail.com>
2022-10-25 09:31:07 -0400

    Updated license function for formatting issues

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,12 +4,12 @@
 // fetchAPI = '"http://api.openweathermap.org/data/2.5/forecast?q='+searchLoc+'&units=imperial&appid='+apiKey+'"'
 //LIVE API
 // fetch(fetchAPI)
-dailyForecast = [];
 //TEST API
 fetch("./assets/test.JSON")
 
 .then (response => response.json())
 .then (function (data) {
+    dailyForecast = [];
     dailyForecast.push(data.city.name);
     makeWeatherObj(data,0);
     dailyForecast.push(weather);
@@ -54,17 +54,28 @@ var nowTemp = dailyForecast[1].temp
 var nowFeelsLike = dailyForecast[1].heatIndex
 var nowHumidity = dailyForecast[1].humidity
 var nowWind = dailyForecast[1].wind
-var nowLowTemp = dailyForecast[1].low
-var nowHighTemp = dailyForecast[1].high
 var nowDetails = dailyForecast[1].weatherDtl
 
 
-var cityDt = cityLoc+' forecast: '+todayDt
+var cityDt = cityLoc+' forecast: '+todayDt;
 console.log(cityDt)
 $("#city-name").append('<h2>'+cityDt+'<h2>');
 
+for (i=2;i<=6;i++) {
+d=i-2;
+dayWkday=moment(dailyForecast[1].dateTxt,"YYYY-MM-DD HH:mm:ss").format("dddd")
+dayTemp=dailyForecast[i].temp
+dayIcon=dailyForecast[i].weatherIcon
+dayHumidity=dailyForecast[i].humidity
+dayWind=dailyForecast[i].wind
+newCard=$('<card>',{class: 'col card m-2', id: 'day'+d,style: "height:30rem", innerHTML: '<div class="card-body"><h4 class="card-title">'+dayWkday+'</h4></br><img src="'+dayIcon+'"class="card-img" id="card-icon" alt="weather icon"><ul class="card-text" id="day-dtls"><li>Temperature: '+dayTemp+'</li><li>Humidity: '+dayHumidity+'</li><li>Wind: '+dayWind+'</li></ul></div>'})
 
-
-
+console.log(newCard);
+futureEl = $("<future>")
+futureEl.append(newCard)
+$("<future>").text("This is a TEST")
+// $('<future>').append('<div>').addClass("col card m-2").attr("id",'day'+d).attr("style","height:30rem").innerHTML('<div class="card-body"><h4 class="card-title">'+dayWkday+'</h4></br><img src="'+dayIcon+'"class="card-img" id="card-icon" alt="weather icon"><ul class="card-text" id="day-dtls"><li>Temperature: '+dayTemp+'</li><li>Humidity: '+dayHumidity+'</li><li>Wind: '+dayWind+'</li></ul></div>')
+}
+return;
 }
 )
