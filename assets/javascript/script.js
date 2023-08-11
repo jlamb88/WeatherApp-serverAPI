edd36277a5509d0fb5a80c506530cd7bb932baf0
Joseph Lamb <jlambpco@gmail.com>
2023-08-11 14:39:51 -0400

    final version

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,21 @@
-require('dotenv').config()
-console.log(process.env)
 
 $("#search-btn").click(() =>
   weatherSearch($("#search-box").val())
 )
 
-
+// window.onscroll = function () { stickyHeader() };
+// var header = document.getElementById("header");
+// var scroll = header.offsetTop;
+// function stickyHeader() {
+//   if (window.pageYOffset >= scroll) {
+//     header.classList.add("sticky")
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
 
 function weatherSearch(loc) {
-  apiKey = process.env.API_KEY;
+  const apiKey = "20f82cbfb698e805cd598e366c3108b2";
   if ($.isNumeric(loc)) {
     fetchAPI =
       'https://api.openweathermap.org/data/2.5/forecast?zip=' + loc + '&units=imperial&appid=' + apiKey;
@@ -28,104 +35,93 @@ function weatherSearch(loc) {
   $('#city-date').empty()
   $('#srch-list').empty()
 
-  fetch(fetchAPI)
+  fetch(fetchAPI)//"./assets/test.JSON"
     .then((response) => response.json())
     .then(function (data) {
-      dailyForecast = [];
-      dailyForecast.push(data.city.name);
-      makeWeatherObj(data, 0);
-      dailyForecast.push(weather);
-      startHour = moment(dailyForecast[1].dateTxt, "YYYY-MM-DD HH:mm:ss")
+      let pos = 0
+      function createWeatherObj(obj) {
+        let {
+          dt_txt: date,
+          main: {
+            temp,
+            feels_like: heatIndex,
+            temp_min: low,
+            temp_max: high,
+            humidity
+          },
+          weather: [
+            {
+              description: details,
+              icon
+            }],
+          wind: {
+            speed: wind
+          },
+          city
+        } = obj
+
+        return { date, temp, heatIndex, low, high, humidity, details, icon, wind, city }
+      }
+      weather = createWeatherObj({ ...data.list[pos], city: data.city.name })
+      formatDt = moment.utc(weather.date).local().format("dddd MMMM, Do YYYY")
+      temp = Math.round(weather.temp)
+      wind = Math.round(weather.wind)
+      heatIndex = Math.round(weather.heatIndex)
+
+      $("#city-icon").append(`<h3 id="city-loc"> ${weather.city} </h3><div class="mb-3"><img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" id="now-icon" alt="weather now icon"></div><h1>${weather.details}</h1>`)
+      $("today-date").append(`<strong>${formatDt}</strong>`)
+      $("#data").append(
+        `<li>${temp}<sup>o</sup></li><li>${weather.humidity}%</li><li>${heatIndex}<sup>o</sup></li><li>${wind} mph</li>`
+      );
+      // $("description").append(`<div class="ml-2"${weather.details}</div>`)
+
+      let startDt = moment.utc(weather.date, "YYYY-MM-DD HH:mm:ss").local()
+      let startHour = moment.utc(weather.date)
         .local()
         .format("HH");
-      startT = 8 - Math.floor((startHour - 12) / 3);
-      for (t = startT; t < 40; t += 8) {
-        makeWeatherObj(data, t);
-        dailyForecast.push(weather);
-      }
-      if (startHour < 15) {
-        makeWeatherObj(data, 39);
-        dailyForecast.push(weather);
-      }
-
-      function makeWeatherObj(data, pos) {
-        weather = {
-          temp: data.list[pos].main.temp,
-          weather: data.list[pos].weather[0].main,
-          weatherIcon: data.list[pos].weather[0].icon,
-          weatherDtl: data.list[pos].weather[0].description,
-          humidity: data.list[pos].main.humidity,
-          heatIndex: data.list[pos].main.feels_like,
-          low: data.list[pos].main.temp_min,
-          high: data.list[pos].main.temp_max,
-          wind: data.list[pos].wind.speed,
-          date: data.list[pos].dt,
-          dateTxt: data.list[pos].dt_txt,
-        };
-        return weather;
-      }
-
-      var cityLoc = dailyForecast[0];
-      var todayDt = moment(dailyForecast[1].dateTxt, "YYYY-MM-DD HH:mm:ss"
-      ).format("dddd MMMM, Do YYYY");
-      var nowIcon = dailyForecast[1].weatherIcon;
-      var nowTemp = dailyForecast[1].temp;
-      var nowFeelsLike = dailyForecast[1].heatIndex;
-      var nowHumidity = dailyForecast[1].humidity;
-      var nowWind = dailyForecast[1].wind;
-      var nowDetails = dailyForecast[1].weatherDtl;
-
-      $("#city-date").append('<h3 id="city-loc">' + cityLoc + "</h3>");
-      $("#city-date").append(
-        '<img src="http://openweathermap.org/img/wn/' +
-        nowIcon +
-        '@2x.png" id="now-icon" alt="weather now icon"></br>'
-      );
-      $("#city-date").append("<today>" + todayDt + "</today>");
-      $("#data").append(
-        "<li>" + nowTemp + "<sup>o</sup></li><li>" +
-        nowHumidity + "%</li><li>" +
-        nowFeelsLike + "<sup>o</sup></li><li>" +
-        nowWind + " mph</li>"
-      );
-
-      for (i = 2; i <= 6; i++) {
-        dayWkday = moment(
-          dailyForecast[i].dateTxt, "YYYY-MM-DD HH:mm:ss").format("dddd");
-        dayTemp = dailyForecast[i].temp;
-        dayIcon = dailyForecast[i].weatherIcon;
-        dayHumidity = dailyForecast[i].humidity;
-        dayWind = dailyForecast[i].wind;
-        newCard = $("<card>", {
+      let startT = 9 - Math.floor((startHour - 12) / 3);
+      console.log(startDt, startHour, startT)
+      console.log(data)
+      for (pos = startT; pos < 40; pos += 8) {
+        let weather = createWeatherObj({ ...data.list[pos], location: data.city.name })
+        let weekday = moment.utc(weather.date, "YYYY-MM-DD HH:mm:ss").local().format("ddd");
+        let temp = Math.round(weather.temp)
+        let wind = Math.round(weather.wind)
+        var newCard = $("<card>", {
           class: "col card m-2",
           style: "height: 20rem",
           html:
-            '<div class="card-body"><h5 class="card-title">' +
-            dayWkday +
-            '</h5><img src="http://openweathermap.org/img/wn/' +
-            dayIcon +
-            '@2x.png "class="card-img" id="card-icon" alt="weather icon"><ul class="card-text" id="day-dtls"><li>Temp: ' +
-            dayTemp +
-            "<sup>o</sup></li><li>Humidity: " +
-            dayHumidity +
-            "%</li><li>Wind: " +
-            dayWind +
-            " mph</li></ul></div>",
+            `<div class="card-body"><h5 class="card-title">${weekday}</h5><div class="mb-4" id="card-icon"><img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" class="card-img" alt="weather icon"></div><ul class="card-text" id="day-dtls"><li>Temp: ${temp}<sup>o</sup></li><li>Humidity: ${weather.humidity}%</li><li>Wind: ${wind} mph</li></ul></div>`,
         });
         $("future").append(newCard);
       }
+      if (startHour < 20) {
+        let weather = createWeatherObj({ ...data.list[39], location: data.city.name })
+        let weekday = moment.utc(weather.date, "YYYY-MM-DD HH:mm:ss").local().format("ddd");
+        let temp = Math.round(weather.temp)
+        let wind = Math.round(weather.wind)
+        var newCard = $("<card>", {
+          class: "col card m-2",
+          style: "height: 20rem",
+          html:
+            `<div class="card-body"><h5 class="card-title">${weekday}</h5><div class="mb-4" id="card-icon"><img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" class="card-img" id="card-icon" alt="weather icon"></div><ul class="card-text" id="day-dtls"><li>Temp: ${temp}<sup>o</sup></li><li>Humidity: ${weather.humidity}%</li><li>Wind: ${wind} mph</li></ul></div>`,
+        });
+        $("future").append(newCard);
 
+      }
 
-      srchHist = localStorage.getItem("history") || "[]"
+      let srchHist = localStorage.getItem("history") || "[]"
       srchHist = JSON.parse(srchHist)
-      if (!srchHist.includes(cityLoc)) {
-        srchHist.unshift(cityLoc)
+      if (!srchHist.includes(weather.city)) {
+        srchHist.unshift(weather.city)
       }
       localStorage.setItem("history", JSON.stringify(srchHist));
+      console.log(srchHist)
 
-      for (i = 0; i < srchHist.length; i++) {
+      for (const history of srchHist) {
+        console.log(history)
         list = $("<li>", {
-          html: srchHist[i]
+          html: `${history}`
         })
         $('#srch-list').append(list)
       }
@@ -136,6 +132,6 @@ function weatherSearch(loc) {
 function checkState(test) {
   stateArr = [
     'AL', 'AK', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'IL', 'IN', 'IA', 'ID', 'KS', 'KY', 'LA', 'ME', 'MA', 'MD', 'MI', 'MN', 'MS', 'MO', 'NC', 'NE', 'ND', 'NV', 'NM', 'NJ', 'NY', 'NH', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WY', 'WA', 'WI', 'WV'];
-  console.log(stateArr.includes(test))
+  test = test.toUpperCase()
   return stateArr.includes(test);
 };
